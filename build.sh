0ee7d6324dc40720847ccb82697126da5146e706
artfin <artfin@mail.ru>
2024-12-10 16:53:06 +0300

    Start of the project

---
+++ build.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+set -xe
+
+INC="-I/home/artfin/Desktop/lib/sundials-5.2.0/instdir/include"
+LIB="/home/artfin/Desktop/lib/sundials-5.2.0/instdir/lib/libsundials_nvecserial.a /home/artfin/Desktop/lib/sundials-5.2.0/instdir/lib/libsundials_cvode.a"
+gcc $INC -Wall -Wextra main.c -o main.exe -lm $LIB 
