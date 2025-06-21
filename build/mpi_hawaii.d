c9d5595dd5a5c93c8791537bfa65506e154cb33f
QwanXiong <danichist@yandex.ru>
2025-06-21 11:06:34 +0300

    Added Makefile_QX

---
+++ build/mpi_hawaii.d
@@ -0,0 +1,171 @@
+build/mpi_hawaii.o: hawaii.c /usr/include/stdc-predef.h hawaii.h \
+ /usr/include/assert.h /usr/include/features.h \
+ /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h /usr/include/errno.h \
+ /usr/include/x86_64-linux-gnu/bits/errno.h /usr/include/linux/errno.h \
+ /usr/include/x86_64-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdbool.h /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h /usr/include/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h /usr/include/string.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/include/regex.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_tm.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_itimerspec.h \
+ /usr/include/unistd.h /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+ /usr/include/x86_64-linux-gnu/bits/environments.h \
+ /usr/include/x86_64-linux-gnu/bits/confname.h \
+ /usr/include/x86_64-linux-gnu/bits/getopt_posix.h \
+ /usr/include/x86_64-linux-gnu/bits/getopt_core.h \
+ /usr/include/x86_64-linux-gnu/bits/unistd_ext.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h \
+ /home/qwan/include/gsl/gsl_histogram.h /home/qwan/include/gsl/gsl_blas.h \
+ /home/qwan/include/gsl/gsl_vector.h \
+ /home/qwan/include/gsl/gsl_vector_complex_long_double.h \
+ /home/qwan/include/gsl/gsl_types.h /home/qwan/include/gsl/gsl_errno.h \
+ /home/qwan/include/gsl/gsl_complex.h \
+ /home/qwan/include/gsl/gsl_check_range.h \
+ /home/qwan/include/gsl/gsl_vector_long_double.h \
+ /home/qwan/include/gsl/gsl_inline.h \
+ /home/qwan/include/gsl/gsl_block_long_double.h \
+ /home/qwan/include/gsl/gsl_vector_complex.h \
+ /home/qwan/include/gsl/gsl_block_complex_long_double.h \
+ /home/qwan/include/gsl/gsl_vector_complex_double.h \
+ /home/qwan/include/gsl/gsl_vector_double.h \
+ /home/qwan/include/gsl/gsl_block_double.h \
+ /home/qwan/include/gsl/gsl_block_complex_double.h \
+ /home/qwan/include/gsl/gsl_vector_complex_float.h \
+ /home/qwan/include/gsl/gsl_vector_float.h \
+ /home/qwan/include/gsl/gsl_block_float.h \
+ /home/qwan/include/gsl/gsl_block_complex_float.h \
+ /home/qwan/include/gsl/gsl_vector_ulong.h \
+ /home/qwan/include/gsl/gsl_block_ulong.h \
+ /home/qwan/include/gsl/gsl_vector_long.h \
+ /home/qwan/include/gsl/gsl_block_long.h \
+ /home/qwan/include/gsl/gsl_vector_uint.h \
+ /home/qwan/include/gsl/gsl_block_uint.h \
+ /home/qwan/include/gsl/gsl_vector_int.h \
+ /home/qwan/include/gsl/gsl_block_int.h \
+ /home/qwan/include/gsl/gsl_vector_ushort.h \
+ /home/qwan/include/gsl/gsl_block_ushort.h \
+ /home/qwan/include/gsl/gsl_vector_short.h \
+ /home/qwan/include/gsl/gsl_block_short.h \
+ /home/qwan/include/gsl/gsl_vector_uchar.h \
+ /home/qwan/include/gsl/gsl_block_uchar.h \
+ /home/qwan/include/gsl/gsl_vector_char.h \
+ /home/qwan/include/gsl/gsl_block_char.h \
+ /home/qwan/include/gsl/gsl_matrix.h \
+ /home/qwan/include/gsl/gsl_matrix_complex_long_double.h \
+ /home/qwan/include/gsl/gsl_blas_types.h \
+ /home/qwan/include/gsl/gsl_cblas.h \
+ /home/qwan/include/gsl/gsl_matrix_complex_double.h \
+ /home/qwan/include/gsl/gsl_matrix_complex_float.h \
+ /home/qwan/include/gsl/gsl_matrix_long_double.h \
+ /home/qwan/include/gsl/gsl_matrix_double.h \
+ /home/qwan/include/gsl/gsl_matrix_float.h \
+ /home/qwan/include/gsl/gsl_matrix_ulong.h \
+ /home/qwan/include/gsl/gsl_matrix_long.h \
+ /home/qwan/include/gsl/gsl_matrix_uint.h \
+ /home/qwan/include/gsl/gsl_matrix_int.h \
+ /home/qwan/include/gsl/gsl_matrix_ushort.h \
+ /home/qwan/include/gsl/gsl_matrix_short.h \
+ /home/qwan/include/gsl/gsl_matrix_uchar.h \
+ /home/qwan/include/gsl/gsl_matrix_char.h \
+ /home/qwan/include/gsl/gsl_linalg.h /home/qwan/include/gsl/gsl_mode.h \
+ /home/qwan/include/gsl/gsl_permutation.h \
+ /home/qwan/include/gsl/gsl_math.h /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /home/qwan/include/gsl/gsl_sys.h /home/qwan/include/gsl/gsl_machine.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/pthread_stack_min-dynamic.h \
+ /usr/include/x86_64-linux-gnu/bits/pthread_stack_min.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/float.h \
+ /home/qwan/include/gsl/gsl_precision.h /home/qwan/include/gsl/gsl_nan.h \
+ /home/qwan/include/gsl/gsl_pow_int.h /home/qwan/include/gsl/gsl_minmax.h \
+ /home/qwan/include/gsl/gsl_multifit_nlinear.h \
+ /home/qwan/include/gsl/gsl_spline.h /home/qwan/include/gsl/gsl_interp.h \
+ /home/qwan/include/gsl/gsl_fft_real.h /home/qwan/include/gsl/gsl_fft.h \
+ /home/qwan/include/gsl/gsl_fft_complex.h \
+ /home/qwan/include/gsl/gsl_fft_halfcomplex.h \
+ /home/qwan/include/gsl/gsl_rng.h /home/qwan/include/gsl/gsl_randist.h \
+ /usr/include/complex.h /usr/include/x86_64-linux-gnu/bits/mathdef.h \
+ /usr/include/x86_64-linux-gnu/bits/cmathcalls.h mtwist.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+ /home/qwan/lib/sundials-5.2.0/include/cvode/cvode.h \
+ /home/qwan/lib/sundials-5.2.0/include/sundials/sundials_nvector.h \
+ /home/qwan/lib/sundials-5.2.0/include/sundials/sundials_types.h \
+ /home/qwan/lib/sundials-5.2.0/include/sundials/sundials_config.h \
+ /home/qwan/lib/sundials-5.2.0/include/sundials/sundials_nonlinearsolver.h \
+ /home/qwan/lib/sundials-5.2.0/include/cvode/cvode_ls.h \
+ /home/qwan/lib/sundials-5.2.0/include/sundials/sundials_direct.h \
+ /home/qwan/lib/sundials-5.2.0/include/sundials/sundials_iterative.h \
+ /home/qwan/lib/sundials-5.2.0/include/sundials/sundials_linearsolver.h \
+ /home/qwan/lib/sundials-5.2.0/include/sundials/sundials_matrix.h \
+ /home/qwan/lib/sundials-5.2.0/include/nvector/nvector_serial.h array.h \
+ arena.h constants.h trajectory.h \
+ /home/qwan/lib/sundials-5.2.0/include/sunmatrix/sunmatrix_dense.h \
+ /home/qwan/lib/sundials-5.2.0/include/sunlinsol/sunlinsol_dense.h \
+ /home/qwan/lib/sundials-5.2.0/include/sundials/sundials_dense.h \
+ /home/qwan/lib/sundials-5.2.0/include/cvode/cvode_direct.h \
+ angles_handler.hpp hep_hawaii.h
